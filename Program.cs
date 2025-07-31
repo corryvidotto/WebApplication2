967743d19cecd465361acfd40de72026b6cd5e71
corry <corry@DESKTOP-3H07OJF>
2025-07-31 09:18:23 -0400

    removed space for test checkin to Git

--- Program.cs
+++ Program.cs
@@ -15,7 +15,6 @@ builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(opt =>
